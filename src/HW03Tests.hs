4f831f73161a9b837ec5106c9365490a5a9dabcf
Crul <rauljavier.vila@gmail.com>
2019-02-18 17:16:23 +0100

    HW03 skeleton
    HW03Tests.hs from https://github.com/ROKT-CIS-194/cis-194-homework/blob/master/2015/src/CIS194/ClaudioN/HW03Tests.hs

---
+++ src/HW03Tests.hs
@@ -0,0 +1,39 @@
+module HW03Tests where
+
+import HW03
+import Testing
+
+-- Exercise 1 -----------------------------------------
+
+ex1Tests :: [Test]
+ex1Tests = []
+
+-- Exercise 2 -----------------------------------------
+
+ex2Tests :: [Test]
+ex2Tests = []
+
+-- Exercise 3 -----------------------------------------
+
+ex3Tests :: [Test]
+ex3Tests = []
+
+-- Exercise 4 -----------------------------------------
+
+ex4Tests :: [Test]
+ex4Tests = []
+
+-- Example Programs -----------------------------------------
+
+progTests :: [Test]
+progTests = []
+
+-- All Tests ------------------------------------------
+
+allTests :: [Test]
+allTests = concat [ ex1Tests
+                  , ex2Tests
+                  , ex3Tests
+                  , ex4Tests
+                  , progTests
+                  ]
