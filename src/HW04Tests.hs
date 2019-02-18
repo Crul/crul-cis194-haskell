6fe3ea57da937bc40ddedc5224b44c8ae461f183
Crul <rauljavier.vila@gmail.com>
2019-02-18 20:11:01 +0100

    HW04 skeleton
    HW04Tests.hs from https://github.com/ROKT-CIS-194/cis-194-homework/blob/master/2015/src/CIS194/ClaudioN/HW04Tests.hs

---
+++ src/HW04Tests.hs
@@ -0,0 +1,53 @@
+module HW04Tests where
+
+import HW04
+import Testing
+
+-- Exercise 1 -----------------------------------------
+
+-- Exercise 2 -----------------------------------------
+
+ex2Tests :: [Test]
+ex2Tests = []
+
+
+-- Exercise 3 -----------------------------------------
+
+ex3Tests :: [Test]
+ex3Tests = []
+
+
+-- Exercise 4 -----------------------------------------
+
+ex4Tests :: [Test]
+ex4Tests = []
+
+
+-- Exercise 5 -----------------------------------------
+
+ex5Tests :: [Test]
+ex5Tests = []
+
+
+-- Exercise 7 -----------------------------------------
+
+ex7Tests :: [Test]
+ex7Tests = []
+
+
+-- Exercise 9 -----------------------------------------
+
+ex9Tests :: [Test]
+ex9Tests = []
+
+
+-- All Tests ------------------------------------------
+
+allTests :: [Test]
+allTests = concat [ ex2Tests,
+                    ex3Tests,
+                    ex4Tests,
+                    ex5Tests,
+                    ex7Tests,
+                    ex9Tests
+                  ]
